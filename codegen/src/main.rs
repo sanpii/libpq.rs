6f1ae54b592a8d15e70dad4df955fb4404674fe6
Sanpi <sanpi@homecomputing.fr>
2020-04-21 18:44:55 +0200

    [codegen] Removes unuseds sqlstate

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,27 +1,5 @@
-mod sqlstate;
 mod type_gen;
 
 fn main() {
     type_gen::build(concat!(env!("CARGO_MANIFEST_DIR"), "/../src/ty/gen.rs"));
-}
-
-fn snake_to_camel(s: &str) -> String {
-    let mut out = String::new();
-
-    let mut upper = true;
-    for ch in s.chars() {
-        if ch == '_' {
-            upper = true;
-        } else {
-            let ch = if upper {
-                upper = false;
-                ch.to_ascii_uppercase()
-            } else {
-                ch
-            };
-            out.push(ch);
-        }
-    }
-
-    out
 }
