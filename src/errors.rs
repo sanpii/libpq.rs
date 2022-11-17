83ae606d83e5734fd408aeeb6f5c871926338128
Sanpi <sanpi@homecomputing.fr>
2022-11-17 10:07:08 +0100

    Uses full qualified name

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,9 @@
-use std::num::ParseIntError;
-
 pub type Result<T = ()> = std::result::Result<T, Error>;
 
 #[derive(Debug, Eq, PartialEq, thiserror::Error)]
 pub enum Error {
     #[error("{0}")]
-    ParseIntError(#[from] ParseIntError),
+    ParseIntError(#[from] std::num::ParseIntError),
     #[error("{0}")]
     NulError(#[from] std::ffi::NulError),
     #[error("{0}")]
