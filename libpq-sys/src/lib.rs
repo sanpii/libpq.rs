e39b1df69566c872cc7b3923b1d7b158eddbfba6
Sanpi <sanpi@homecomputing.fr>
2024-08-19 13:48:18 +0200

    Silents bindgen clippy warnings

--- libpq-sys/src/lib.rs
+++ libpq-sys/src/lib.rs
@@ -1,8 +1,8 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-#![allow(clippy::redundant_static_lifetimes)]
-#![allow(clippy::upper_case_acronyms)]
+#![allow(clippy::unnecessary_operation)]
+#![allow(clippy::identity_op)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
