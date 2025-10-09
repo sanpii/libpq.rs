72d0174dbaed92aad1b5801931935672664e6a97
Sanpi <sanpi@homecomputing.fr>
2025-10-09 10:43:20 +0200

    Adds Connection::request_cancel

--- src/connection/_cancel.rs
+++ src/connection/_cancel.rs
@@ -12,4 +12,20 @@ impl Connection {
     pub fn cancel(&self) -> crate::connection::Cancel {
         unsafe { pq_sys::PQgetCancel(self.into()) }.into()
     }
+
+    /**
+     * Requests that the server abandon processing of the current command.
+     *
+     * See [PQrequestCancel](https://www.postgresql.org/docs/18/libpq-cancel.html#LIBPQ-PQREQUESTCANCEL).
+     */
+    #[deprecated(note = "Use Cancel::blocking() instead")]
+    pub fn request_cancel(&self) -> crate::errors::Result {
+        let sucess = unsafe { pq_sys::PQrequestCancel(self.into()) };
+
+        if sucess == 1 {
+            Ok(())
+        } else {
+            self.error()
+        }
+    }
 }
